88178aa5cf8bf7027840a51d8e87915073666871
t0bi-0ss <183855820+t0bi-0ss@users.noreply.github.com>
2026-02-19 11:43:54 +0100

    Update

---
+++ src/task_tracker/core.py
@@ -0,0 +1,52 @@
+"""
+The user should be able to:
+
+Add, Update, and Delete tasks
+
+Mark a task as in progress or done
+
+List all tasks
+
+List all tasks that are done
+
+List all tasks that are not done
+
+List all tasks that are in progress
+________________________________________________________________________
+Use positional arguments in command line to accept user inputs.
+
+Use a JSON file to store the tasks in the current directory.
+
+The JSON file should be created if it does not exist.
+
+Use the native file system module of your programming language to interact with the JSON file.
+"""
+
+# Class to manage all tasks
+
+class TaskManager:
+    # Add task
+    
+    # Update task
+
+    # Delete task
+
+    # Mark task as in progress
+
+    # Mark task as done
+
+    # List all tasks that are done
+
+    # List all tasks that are not done
+
+    # List all tasks that are in progress
+    pass
+
+
+# Task class
+
+class Task:
+    # Should be initialized as "done = False" "in_progress = False"
+
+    # 
+    pass
